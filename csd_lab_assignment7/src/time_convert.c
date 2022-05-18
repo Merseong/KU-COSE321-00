2979c1e15c893e0b3e665aa51ab6aa46f26f62d7
Merseong <esc990720@korea.ac.kr>
2022-05-19 02:15:19 +0900

    Fix counting problems
    Assignment7

--- csd_lab_assignment7/src/time_convert.c
+++ csd_lab_assignment7/src/time_convert.c
@@ -2,12 +2,12 @@ char* time_convert(char* addr, unsigned elapsed)
 {
 	char* addr_copy = addr;
 
-	unsigned hour = elapsed / 3600;
+	unsigned hour = (elapsed / 3600) % 100;
 	*addr_copy++ = hour / 10 + '0';
 	*addr_copy++ = hour % 10 + '0';
 	*addr_copy++ = ':';
 
-	unsigned minute = elapsed / 60;
+	unsigned minute = (elapsed / 60) % 60;
 	*addr_copy++ = minute / 10 + '0';
 	*addr_copy++ = minute % 10 + '0';
 	*addr_copy++ = ':';
