9a3c64b986e767529632d8e3da146461f635032d
Merseong <esc990720@korea.ac.kr>
2022-05-18 23:54:38 +0900

    Implement assignment7
    Assignment7

---
+++ csd_lab_assignment7/src/time_convert.c
@@ -0,0 +1,20 @@
+char* time_convert(char* addr, unsigned elapsed)
+{
+	char* addr_copy = addr;
+
+	unsigned hour = elapsed / 3600;
+	*addr_copy++ = hour / 10 + '0';
+	*addr_copy++ = hour % 10 + '0';
+	*addr_copy++ = ':';
+
+	unsigned minute = elapsed / 60;
+	*addr_copy++ = minute / 10 + '0';
+	*addr_copy++ = minute % 10 + '0';
+	*addr_copy++ = ':';
+
+	unsigned second = elapsed % 60;
+	*addr_copy++ = second / 10 + '0';
+	*addr_copy++ = second % 10 + '0';
+
+	return addr;
+}
